987aa49379b2b6506029d57cd2859d150e7bb45f
Don Mallory <dmm333@gmail.com>
2020-08-22 11:08:30 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    results = []
+    for i in 3..limit-1
+        if i % 3 == 0 || i % 5 == 0
+            results << i
+        end
+    end
+    results
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+class Multiples
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        results = []
+        for i in 3..@limit-1
+            if i % 3 == 0 || i % 5 == 0
+                results << i
+            end
+        end
+        results
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
